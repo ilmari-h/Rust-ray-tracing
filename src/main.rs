184378e66f62a67586fdba0d23ae89edcb18bd92
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2021-09-07 18:31:21 +0300

    Save output image as png.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate indicatif;
+extern crate image;
 
 use std::sync::Arc;
-use std::fs::File;
-use std::io::prelude::*;
 mod camera;
 use camera::Camera;
 mod vec;
-use vec::{Vec3, Color, Point3};
+use vec::*;
 mod ray;
 use ray::Ray;
 mod hittables;
@@ -18,13 +17,14 @@ mod texture;
 use texture::Texture;
 mod util;
 use util::randomf;
+use image::{ImageBuffer};
 
 // NOTE: add concurrency
 
 const ASPECT_R: f64 = 16.0 / 9.0;
-const IWIDTH: usize = 1280; // Making this big causes stack overflow because of array size IWIDTH*IHEIGHT.
+const IWIDTH: usize = 1920; // Making this big causes stack overflow because of array size IWIDTH*IHEIGHT.
 const IHEIGHT: usize = ( IWIDTH as f32 / ASPECT_R as f32 ) as usize ;
-const BLACK: Vec3 = Vec3{x:0.0,y:0.0,z:0.0};
+const BLACK: Vec3 = v3!(0.0,0.0,0.0);
 const RAY_RECURSION_DEPTH: u32 = 50;
 const ALMOST_ZERO: f64 = 0.001;
 type Pixel = [i32; 3];
@@ -49,38 +49,33 @@ fn ray_color(ray: &Ray, objects: &HittableVec, depth: u32) -> Color {
 
 fn main() -> () {
     let test_tex = Texture::new("img/tile2_diff.jpg".to_string(), 1.0);
-
     let brown_matte = Arc::new( Diffuse{ attenuation: Color{x: 0.51, y: 0.31, z: 0.21} });
     let clear_metal = Arc::new(Metal{ attenuation: Color{x: 1.0, y: 1.0, z: 1.0}, fuzz: 0.0 } );
     let blue_metal = Arc::new( Metal{ attenuation: Color{x: 0.20, y: 0.20, z: 0.70}, fuzz: 0.0 });
     let green_metal = Arc::new (Metal{ attenuation: Color{x: 0.0, y: 0.70, z: 0.0}, fuzz: 0.1 });
+    let pink_metal = Arc::new (Metal{ attenuation: Color{x: 0.92, y: 0.11, z: 0.92}, fuzz: 0.0 });
     let black_metal = Arc::new (Metal{ attenuation: Color{x: 0.1, y: 0.1, z: 0.1} , fuzz: 0.0 } );
     let glass_m = Arc::new(  Dielectric{ index_of_refraction: 1.5 } );
-
     let texture_m = Arc::new( TexMat{ texture: test_tex } );
 
-
     let mut objects = HittableVec::new();
-    objects.push(Box::new(Sphere{center: Vec3::new(-0.51, 0.0, -1.0), radius: 0.5, material: brown_matte.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(0.51, 0.0, -1.0), radius: 0.5, material: blue_metal.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(-0.1, -0.35, 0.2), radius: 0.15, material: clear_metal.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(-1.2, 0.0, 0.0), radius: 0.5, material: green_metal.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(1.2, 0.0, 0.0), radius: 0.5, material: glass_m.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(0.5, -0.35, -0.3), radius: 0.15, material: black_metal.clone()}));
-    objects.push(Box::new(Sphere{center: Vec3::new(-0.7, -0.35, -0.48), radius: 0.15, material: green_metal.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(-0.51, 0.0, -1.0), radius: 0.5, material: brown_matte.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(0.51, 0.0, -1.0), radius: 0.5, material: blue_metal.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(-0.1, -0.35, 0.2), radius: 0.15, material: clear_metal.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(-1.2, 0.0, 0.0), radius: 0.5, material: green_metal.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(1.2, 0.0, 0.0), radius: 0.5, material: glass_m.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(0.5, -0.35, -0.3), radius: 0.15, material: black_metal.clone()}));
+    objects.push(Box::new(Sphere{center: v3!(-0.6, -0.35, -0.4), radius: 0.15, material: pink_metal.clone()}));
     objects.push(Box::new(Plane{y:-0.5, material: texture_m.clone() } ) );
 
-    let mut im: PArr = vec![];
-
-    println!("Rendering image...");
-    im = render(&im, &objects); // TODO combine these two
-    save_image(&im);
+    let im = render(&objects);
+    save_image(&im).unwrap();
 }
 
-fn render(img: &PArr, objects: &HittableVec) -> PArr {
+fn render(objects: &HittableVec) -> PArr {
 
     // Camera
-    let camera = Camera::new( ASPECT_R, 60.0, Vec3::new(0.0, 0.75, 3.0) );
+    let camera = Camera::new( ASPECT_R, 60.0, v3!(-1.0, 0.75, 3.0), v3!(-10.0,-14.0,0.0) );
     let pixel_samples = 100;
     let bar = ProgressBar::new(IHEIGHT as u64);
     bar.set_style(ProgressStyle::default_bar()
@@ -89,8 +84,8 @@ fn render(img: &PArr, objects: &HittableVec) -> PArr {
 
     let mut x = 0;
     let mut y = IHEIGHT;
-    let mut n_img: PArr = img.to_owned();
-    for i in 0..IWIDTH*IHEIGHT {
+    let mut n_img: PArr = vec![];
+    for _ in 0..IWIDTH*IHEIGHT {
 
         // Create rays.
         let mut color = Color::new(0.0,0.0,0.0);
@@ -111,7 +106,7 @@ fn render(img: &PArr, objects: &HittableVec) -> PArr {
 
         // Scale by samples and gamma correction
         let scale = 1.0 / pixel_samples as f64;
-        color = Vec3::new(
+        color = v3!(
             (color.x * scale).sqrt(),
             (color.y * scale).sqrt(),
             (color.z * scale).sqrt()
@@ -133,14 +128,14 @@ fn render(img: &PArr, objects: &HittableVec) -> PArr {
 }
 
 fn save_image(img: &PArr) -> std::io::Result<()> {
-    let mut f = File::create("img.ppm")?;
-    let header: String = format!("P3\n{} {}\n{}\n",IWIDTH,IHEIGHT,255);
-    f.write_all(header.as_bytes())?;
-    for i in 0..IWIDTH*IHEIGHT {
-        let px: Pixel = img[i];
-        let px_s: String = format!("{} {} {}\n", px[0], px[1], px[2]);
-        f.write_all(px_s.as_bytes())?;
-    }
+
+    let img = ImageBuffer::from_fn(IWIDTH as u32, IHEIGHT as u32, |x,y| {
+        let i = x as usize + y as usize * IWIDTH;
+        return image::Rgb([img[i][0] as u8, img[i][1] as u8, img[i][2] as u8]);
+    });
+
+    img.save("image.png").unwrap();
+
     Ok(())
 }
 
